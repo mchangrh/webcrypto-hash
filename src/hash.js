91cc4c09a23687f02526b72103e0cef63f62b8dd
Michael C <michael@mchang.name>
2021-08-26 01:20:29 -0400

    initial coomit

---
+++ src/hash.js
@@ -0,0 +1,30 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+async function digestMessage(message, algo) {
+  const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8)
+  const hashBuffer = await crypto.subtle.digest(algo, msgUint8); // hash the message
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // buffer to byte array
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // bytes to hex string
+  return hashHex;
+}
+
+async function multiHash(input, algo, times) {
+  for (let i = 0; i < times; i++) {
+    input = await digestMessage(input, algo);
+  }
+  return input;
+}
+
+async function handleRequest(request) {
+  const { searchParams } = new URL(request.url);
+  const input = searchParams.get('input');
+  const times = searchParams.get('times') ?? 5000;
+  const algo = searchParams.get('algo') ?? "SHA-256";
+  return new Response(await multiHash(input, algo, times));
+}
